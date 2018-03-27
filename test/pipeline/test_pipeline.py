0350fb8a74f4b60f32abeba4ffa64f97290090af
Ching-Tien.Wang <grassking100@gmail.com>
2018-03-27 16:59:16 +0800

    Add test for checking constant metrics can work fine in pipeline

--- test/pipeline/test_pipeline.py
+++ test/pipeline/test_pipeline.py
@@ -2,8 +2,9 @@ import unittest
 import os
 import sys
 import shutil
-sys.path.append(os.path.abspath("~/../../.."))
+import pandas as pd
 import argparse
+sys.path.append(os.path.abspath("~/../../.."))
 from sequence_annotation.pipeline.train_pipeline import TrainSeqAnnPipeline
 from sequence_annotation.worker.model_trainer import ModelTrainer
 class TestPipeline(unittest.TestCase):
@@ -16,6 +17,24 @@ class TestPipeline(unittest.TestCase):
             train_pipeline = TrainSeqAnnPipeline("pipeline_test",user_setting['work_setting_path'],
                                                  user_setting['model_setting_path'],False)
             train_pipeline.execute()
+        except Exception as e:
+            raise e
+            self.fail("There are some unexpected exception occur.")
+        finally:
+            if os.path.exists('result'):
+                shutil.rmtree('result')
+    def test_constant(self):
+        user_setting = {}
+        user_setting['work_setting_path']='~/deep_learning/sequence_annotation/test/pipeline/setting/training_setting.json'
+        user_setting['model_setting_path']='~/deep_learning/sequence_annotation/test/pipeline/setting/model_setting.json'
+        user_setting['train_id']='simple_run'
+        try:
+            train_pipeline = TrainSeqAnnPipeline("pipeline_test",user_setting['work_setting_path'],
+                                                 user_setting['model_setting_path'],False)
+            train_pipeline.execute()
+            data = pd.read_csv('result/pipeline_test/test/result/epoch_3.csv')
+            self.assertEqual((data['constant_layer']==300).tolist(),[True]*3)
+            self.assertEqual((data['val_constant_layer']==300).tolist(),[True]*3)
         except Exception as e:
             raise e
             self.fail("There are some unexpected exception occur.")
