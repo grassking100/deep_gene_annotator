b93d93f8767f52a07fc43a348c1d8f16ad398c63
Ching-Tien.Wang <grassking100@gmail.com>
2018-03-27 17:03:19 +0800

    Add test for checking constant metrics can work fine in pipeline

--- test/pipeline/test_pipeline.py
+++ test/pipeline/test_pipeline.py
@@ -12,35 +12,35 @@ class TestPipeline(unittest.TestCase):
         user_setting = {}
         user_setting['work_setting_path']='~/deep_learning/sequence_annotation/test/pipeline/setting/training_setting.json'
         user_setting['model_setting_path']='~/deep_learning/sequence_annotation/test/pipeline/setting/model_setting.json'
-        user_setting['train_id']='simple_run'
+        user_setting['train_id']='test_runable'
         try:
-            train_pipeline = TrainSeqAnnPipeline("pipeline_test",user_setting['work_setting_path'],
+            train_pipeline = TrainSeqAnnPipeline(user_setting['train_id'],user_setting['work_setting_path'],
                                                  user_setting['model_setting_path'],False)
             train_pipeline.execute()
         except Exception as e:
             raise e
             self.fail("There are some unexpected exception occur.")
         finally:
-            if os.path.exists('result'):
-                shutil.rmtree('result')
+            if os.path.exists('result/'+user_setting['train_id']):
+                shutil.rmtree('result/'+user_setting['train_id'])
     def test_constant(self):
         user_setting = {}
         user_setting['work_setting_path']='~/deep_learning/sequence_annotation/test/pipeline/setting/training_setting.json'
         user_setting['model_setting_path']='~/deep_learning/sequence_annotation/test/pipeline/setting/model_setting.json'
-        user_setting['train_id']='simple_run'
+        user_setting['train_id']='test_constant'
         try:
-            train_pipeline = TrainSeqAnnPipeline("pipeline_test",user_setting['work_setting_path'],
+            train_pipeline = TrainSeqAnnPipeline(user_setting['train_id'],user_setting['work_setting_path'],
                                                  user_setting['model_setting_path'],False)
             train_pipeline.execute()
-            data = pd.read_csv('result/pipeline_test/test/result/epoch_3.csv')
+            data = pd.read_csv('result/'+user_setting['train_id']+'/test/result/epoch_3.csv')
             self.assertEqual((data['constant_layer']==300).tolist(),[True]*3)
             self.assertEqual((data['val_constant_layer']==300).tolist(),[True]*3)
         except Exception as e:
             raise e
             self.fail("There are some unexpected exception occur.")
         finally:
-            if os.path.exists('result'):
-                shutil.rmtree('result')
+            if os.path.exists('result/'+user_setting['train_id']):
+                shutil.rmtree('result/'+user_setting['train_id'])
 if __name__=="__main__":
     unittest.TestSuite()
     unittest.TestLoader().loadTestsFromTestCase(TestPipeline)
