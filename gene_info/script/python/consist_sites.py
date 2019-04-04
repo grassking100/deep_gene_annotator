f2a3ea79de793a15503cea4e002b5989bd212f37
Ching-Tien Wang <grassking100@gmail.com>
2019-04-04 15:19:04 +0000

    Try to fixed gene_info problem

--- gene_info/script/python/consist_sites.py
+++ gene_info/script/python/consist_sites.py
@@ -64,22 +64,35 @@ if __name__ == "__main__":
         inner_gro_sites['gro_source'] = 'inner'
         long_dist_gro_sites['gro_source'] = 'long_dist'
         orf_inner_gro_sites['gro_source'] = 'orf_inner'
-        merged_gro_sites_ = pd.concat([dist_gro_sites,inner_gro_sites,long_dist_gro_sites])
-        merged_gro_sites = consist(merged_gro_sites_,'ref_name','tag_count',True)
+        merged_gro_sites = pd.concat([dist_gro_sites,inner_gro_sites,long_dist_gro_sites])
+        
         dist_cleavage_sites['cleavage_source'] = 'dist'
         inner_cleavage_sites['cleavage_source'] = 'inner'
         long_dist_cleavage_sites['cleavage_source'] = 'long_dist'
         orf_inner_cleavage_sites['cleavage_source'] = 'orf_inner'
-        merged_cleavage_sites_ = pd.concat([dist_cleavage_sites,inner_cleavage_sites,long_dist_cleavage_sites])
-        merged_cleavage_sites = consist(merged_cleavage_sites_,'ref_name','read_count',True)
+        
+        merged_cleavage_sites = pd.concat([dist_cleavage_sites,inner_cleavage_sites,long_dist_cleavage_sites])
+        ##
+        gro_names = get_gene_names(merged_gro_sites,'ref_name',id_convert)
+        merged_gro_sites = merged_gro_sites.assign(gene_id=pd.Series(gro_names).values)
+        cs_names = get_gene_names(merged_cleavage_sites,'ref_name',id_convert)
+        merged_cleavage_sites = merged_cleavage_sites.assign(gene_id=pd.Series(cs_names).values)
+        ##
+
         ###Clean data without invalid sites###
         print('Clean and export data')
-        safe_merged_gro_site = merged_gro_sites[merged_gro_sites['gro_source'].isin(['dist','inner'])]
-        safe_merged_cleavage_sites = merged_cleavage_sites[merged_cleavage_sites['cleavage_source'].isin(['dist','inner'])]
+        long_dist_gro_site_id = set(merged_gro_sites[merged_gro_sites['gro_source'].isin(['long_dist'])]['gene_id'])
+        
+        safe_merged_gro_site = merged_gro_sites[merged_gro_sites['gro_source'].isin(['dist','inner']) ]
+        
+        long_dist_cleavage_site_id = set(merged_cleavage_sites[merged_cleavage_sites['cleavage_source'].isin(['long_dist'])]['gene_id'])
+        safe_merged_cleavage_sites = merged_cleavage_sites[merged_cleavage_sites['cleavage_source'].isin(['dist','inner'])& merged_cleavage_sites['gene_id'].isin(long_dist_cleavage_site_id)]
+        
+        
+        #safe_merged_gro_site = consist(safe_merged_gro_site,'gene_id','tag_count',True)
+        #safe_merged_cleavage_sites = consist(safe_merged_cleavage_sites,'gene_id','read_count',True)
+        
         ###Write data###
-        gro_names = get_gene_names(safe_merged_gro_site,'ref_name',id_convert)
-        safe_merged_gro_site = safe_merged_gro_site.assign(gene_id=pd.Series(gro_names).values)
-        cs_names = get_gene_names(safe_merged_cleavage_sites,'ref_name',id_convert)
-        safe_merged_cleavage_sites = safe_merged_cleavage_sites.assign(gene_id=pd.Series(cs_names).values)
+
         safe_merged_gro_site.to_csv(safe_merged_gro_sites_path,sep='\t',index=False)
         safe_merged_cleavage_sites.to_csv(safe_merged_cleavage_sites_path,sep='\t',index=False)
