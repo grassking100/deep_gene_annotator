cc4aa2d51704855e86e276b72f898d0aada19d70
Ching-Tien Wang <grassking100@gmail.com>
2019-12-26 09:31:37 +0000

    Fix TestDataContainer

--- test/process/test_data_container.py
+++ test/process/test_data_container.py
@@ -7,13 +7,14 @@ from sequence_annotation.utils.exception import DimensionNotSatisfy
 class TestDataContainer(unittest.TestCase):
     def test_ann_seq_data(self):
         try:
+            CHANNEL_ORDER = ['Black','White']
             ann_seqs = AnnSeqContainer()
-            ann_seqs.ANN_TYPES=['Black','White']
+            ann_seqs.ANN_TYPES=CHANNEL_ORDER
             ann_seq = AnnSequence()
             ann_seq.length = 5
             ann_seq.id='A'
             ann_seq.strand = 'plus'
-            ann_seq.ANN_TYPES = ['Black','White']
+            ann_seq.ANN_TYPES = CHANNEL_ORDER
             ann_seq.init_space()
             ann_seq.set_ann('Black',1,3,4)
             ann_seq.set_ann('White',1,0,2)
@@ -22,13 +23,14 @@ class TestDataContainer(unittest.TestCase):
             ann_seq2.length = 4
             ann_seq2.id='B'
             ann_seq2.strand = 'plus'
-            ann_seq2.ANN_TYPES = ['Black','White']
+            ann_seq2.ANN_TYPES = CHANNEL_ORDER
             ann_seq2.init_space()
             ann_seq2.set_ann('Black',1,3,3)
             ann_seq2.set_ann('White',1,0,2)
             ann_seqs.add(ann_seq2)
-            data = AnnSeqProcessor({'training':{'inputs':{'A':'AATCG','B':'TTTC'},'answers':ann_seqs}})
-            data.process()
+            data = {'training':{'inputs':{'A':'AATCG','B':'TTTC'},'answers':ann_seqs}}
+            data_processor = AnnSeqProcessor(CHANNEL_ORDER)
+            data_processor.process(data)
         except Exception as e:
             raise e
             self.fail("There are some unexpected exception occur.")
